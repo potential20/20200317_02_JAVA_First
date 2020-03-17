8b98ed79504da6185a9d84bb2a52bbceeddd54b9
potential20 <passion212@nate.com>
2020-03-17 15:11:49 +0900

    연습문제 답 완료

--- app/src/main/java/kr/co/tjoeun/a20200317_02_java_first/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200317_02_java_first/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -20,11 +21,18 @@ public class MainActivity extends AppCompatActivity {
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
 
 
-
-
 //        EditText에 타이핑 => 입력버튼 => 텍스트뷰에 출력
 //        + 입력버튼 누르면 기존 입력내용삭제 ""으로 설정.
 
+        binding.okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String input = binding.inputEdt.getText().toString();
+
+                binding.resultTxt.setText(input);
+                binding.inputEdt.setText("");
+            }
+        });
 
 
 
