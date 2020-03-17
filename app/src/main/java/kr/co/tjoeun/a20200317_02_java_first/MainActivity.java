ca475bd2c3de0147c4ee6887b47db6e0d45b96ea
potential20 <passion212@nate.com>
2020-03-17 15:03:16 +0900

    데이터바인딩 세팅

--- app/src/main/java/kr/co/tjoeun/a20200317_02_java_first/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200317_02_java_first/MainActivity.java
@@ -1,14 +1,34 @@
 package kr.co.tjoeun.a20200317_02_java_first;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import kr.co.tjoeun.a20200317_02_java_first.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+
+
+
+//        EditText에 타이핑 => 입력버튼 => 텍스트뷰에 출력
+//        + 입력버튼 누르면 기존 입력내용삭제 ""으로 설정.
+
+
+
+
+
+
     }
 }
